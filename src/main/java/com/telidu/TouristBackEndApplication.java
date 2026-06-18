ff6331b9924efca332f56fc28921439465a25988
Preetham00x <preetham2006@outlook.com>
2026-06-18 22:24:34 +0530

    usage of restcontroller + datajpa in a single application

---
+++ src/main/java/com/telidu/TouristBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.telidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TouristBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telidu/TouristBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telidu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TouristBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
