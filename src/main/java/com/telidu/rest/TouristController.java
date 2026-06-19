97f385416846586c9ad4ea0452084260bf93d9d9
Preetham00x <preetham2006@outlook.com>
2026-06-19 22:22:41 +0530

    adding a setter injection in the touristcontroller

--- src/main/java/com/telidu/rest/TouristController.java
+++ src/main/java/com/telidu/rest/TouristController.java
@@ -1,5 +1,15 @@
 package com.telidu.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.telidu.service.ITouristService;
+
 public class TouristController {
+	private ITouristService service;
+	
+	@Autowired
+	public void setService(ITouristService service) {
+		this.service=service;
+	}
 
 }
